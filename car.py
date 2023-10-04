271fb6981a62b58caa3538bbd45d5d8e4cf7fa86
Nate-Schaefer <nathanschaefer-schaefer@uiowa.edu>
2023-10-04 11:42:38 -0500

    Some work on refactoring class structures

--- car.py
+++ car.py
@@ -1,10 +1,16 @@
-from abc import ABC, abstractmethod
+from battery.battery import Battery
+from engine.engine import Engine
 
 
-class Car(ABC):
+class Car():
     def __init__(self, last_service_date):
+        self.Engine = Engine
+        self.Battery = Battery
         self.last_service_date = last_service_date
 
     @abstractmethod
     def needs_service(self):
-        pass
+        if self.Engine.needs_service() or self.Battery.needs_service():
+            return True
+        else:
+            return False
