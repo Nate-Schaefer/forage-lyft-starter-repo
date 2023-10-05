60dd40a25b598300cbf5f18c75088e25685beec4
Nate-Schaefer <nathanschaefer-schaefer@uiowa.edu>
2023-10-04 22:15:07 -0500

    fixing few small things based on given example

--- CarFactory.py
+++ CarFactory.py
@@ -8,27 +8,32 @@ from Serviceable.car import Car
 
 class CarFactory():
 
-    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    @staticmethod
+    def create_calliope(last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(last_service_date)
         return Car(engine, battery)
     
-    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    @staticmethod
+    def create_glissade(last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(last_service_date)
         return Car(engine, battery)
 
-    def create_palindrome(self, current_date, last_service_date, warning_light_on):
+    @staticmethod
+    def create_palindrome(last_service_date, warning_light_on):
         engine = SternmanEngine(warning_light_on)
         battery = SpindlerBattery(last_service_date)
         return Car(engine, battery)
     
-    def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    @staticmethod
+    def create_rorschach(last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(last_service_date)
         return Car(engine, battery)
     
-    def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    @staticmethod
+    def create_thovex(last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(last_service_date)
         return Car(engine, battery)
